90d37c0cf6b1310e99030558c8f1a4b2231a061b
witog60248wavelixcom-collab <witog60248wavelix.com@gmail.com>
2025-09-27 03:42:53 +0300

    Add files via upload

---
+++   حماية فول  .lua
@@ -0,0 +1,27 @@
+[
+
+  {
+    "user": "ab1",
+    "pass": "mod1",
+    "vip": false
+  },
+  
+  {
+    "user": "ab2",
+    "pass": "mod2",
+    "vip": false
+  },
+  
+  {
+    "user": "ab3",
+    "pass": "mod3",
+    "vip": false
+  },
+  
+  {
+    "user": "ab4",
+    "pass": "mod4",
+    "vip": false
+  }
+  
+]
