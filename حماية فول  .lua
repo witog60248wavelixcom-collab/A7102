726eb8e64a30ef4031cedc02ee07c210864a2470
witog60248wavelixcom-collab <witog60248wavelix.com@gmail.com>
2025-09-27 03:51:58 +0300

    Update   حماية فول  .lua

--- حماية فول  .lua
+++ حماية فول  .lua
@@ -1,25 +1,25 @@
 [
 
   {
-    "user": "ab1",
+    "user": "BC1",
     "pass": "mod1",
     "vip": false
   },
   
   {
-    "user": "ab2",
+    "user": "BC2",
     "pass": "mod2",
     "vip": false
   },
   
   {
-    "user": "ab3",
+    "user": "BC3",
     "pass": "mod3",
     "vip": false
   },
   
   {
-    "user": "ab4",
+    "user": "BC3",
     "pass": "mod4",
     "vip": false
   }
